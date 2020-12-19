4cb368bfe1ce67fc147331ff157a8da000ab84e9
Alex Petitjean <alex.m.petitjean@gmail.com>
2020-12-18 22:47:18 -0600

    Add some UI for listing emulators

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,74 @@
+import androidx.compose.animation.expandHorizontally
 import androidx.compose.desktop.Window
-import androidx.compose.material.Text
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.Edit
+import androidx.compose.material.icons.twotone.PlayArrow
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.HorizontalAlignmentLine
+import androidx.compose.ui.unit.dp
 
-fun main() = Window {
-    var text by remember { mutableStateOf("Hello, World!") }
-
+fun main() = Window(title = "AViDity") {
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
+        AvidityApp()
     }
 }
+
+@Composable
+fun AvidityApp() {
+    val fakeEmulators = listOf(
+        Emulator("Pixel 2 XL"),
+        Emulator("Nexus 5X"),
+        Emulator("Pixel 4"),
+        Emulator("Pixel 5"),
+    ).let {
+        // Make a lot of them
+        it + it + it + it + it + it + it + it + it + it + it + it + it + it
+    }
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text("Emulators", style = MaterialTheme.typography.h4)
+        Divider()
+        EmulatorList(fakeEmulators)
+    }
+}
+
+@Composable
+fun EmulatorList(
+    emulators: List<Emulator>,
+) {
+    LazyColumnFor(modifier = Modifier.fillMaxWidth(), items = emulators) { emulator ->
+        EmulatorItem(emulator)
+    }
+}
+
+@Composable
+fun EmulatorItem(emulator: Emulator) {
+    Box(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+        Text(emulator.name, modifier = Modifier.align(Alignment.CenterStart))
+
+        Row(modifier = Modifier.align(Alignment.CenterEnd)) {
+            Icon(
+                imageVector = Icons.TwoTone.PlayArrow,
+                modifier = Modifier.padding(4.dp),
+            )
+            Icon(
+                imageVector = Icons.TwoTone.Edit,
+                modifier = Modifier.padding(4.dp),
+            )
+        }
+    }
+
+    Divider()
+}
+
+data class Emulator(
+    val name: String,
+)
