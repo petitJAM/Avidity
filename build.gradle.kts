145f886715b39b52e491f07cc9e6cf19cb1c7b63
Alex Petitjean <alex.m.petitjean@gmail.com>
2020-12-19 11:32:58 -0600

    Update project to use JDK 14 so packaging works
    I can create an msi file now and install it in Windows, which is super
    cool.

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "14"
     kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
 }
 
