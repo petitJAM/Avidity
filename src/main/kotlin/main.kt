be86f8839712f1d7444e551dc139eae24918738c
Alex Petitjean <alex.m.petitjean@gmail.com>
2020-12-20 00:42:06 -0600

    Add dialog for creating new emulator

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,23 +1,28 @@
 import androidx.compose.desktop.Window
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.Add
@@ -27,13 +32,16 @@ import androidx.compose.material.icons.twotone.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.savedinstancestate.savedInstanceState
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.vectorXmlResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import data.Emulator
 import theme.AvidityDarkColorPalette
 import theme.AvidityLightColorPalette
@@ -68,13 +76,29 @@ fun AvidityApp(darkTheme: MutableState<Boolean>) {
         )
     }
 
-    AvidityContent(darkTheme, emulators)
+    val dialogState = remember { mutableStateOf(false) }
+
+    AvidityContent(darkTheme, emulators, dialogState)
+
+    if (dialogState.value) {
+        Dialog(
+            onDismissRequest = { dialogState.value = false }
+        ) {
+            NewEmulatorForm(
+                onEmulatorCreated = {
+                    emulators += it
+                    dialogState.value = false
+                }
+            )
+        }
+    }
 }
 
 @Composable
 fun AvidityContent(
     darkTheme: MutableState<Boolean>,
     emulators: SnapshotStateList<Emulator>,
+    dialogState: MutableState<Boolean>,
 ) {
     Column(modifier = Modifier.fillMaxSize().background(MaterialTheme.colors.background)) {
         TopAppBar(
@@ -104,7 +128,7 @@ fun AvidityContent(
                 text = { Text("Add Emulator") },
                 icon = { Icon(imageVector = Icons.TwoTone.Add) },
                 onClick = {
-                    emulators.add(Emulator("New Emulator"))
+                    dialogState.value = true
                 },
             )
         }
@@ -196,4 +220,41 @@ fun EmulatorItem(
     }
 
     Divider()
+}
+
+@Composable
+fun NewEmulatorForm(
+    onEmulatorCreated: (emulator: Emulator) -> Unit,
+) {
+    val valid = remember { mutableStateOf(false) }
+
+    val name = remember { mutableStateOf(TextFieldValue()) }
+
+    val revalidate = { valid.value = name.value.text.isNotBlank() }
+
+    ScrollableColumn(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = name.value,
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            label = { Text("Name") },
+            onValueChange = { name.value = it; revalidate() },
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            modifier = Modifier.align(Alignment.CenterHorizontally).padding(horizontal = 16.dp),
+            enabled = valid.value,
+            onClick = {
+                val newEmulator = Emulator(name.value.text)
+                onEmulatorCreated(newEmulator)
+            },
+        ) {
+            Text("Create Emulator")
+        }
+    }
 }
